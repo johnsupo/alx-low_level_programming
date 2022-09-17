b96222ed09fe0d1375ea84b3837ed24feec7b531
johnsupo <johnsupo@yahoo.com>
2022-09-17 15:14:17 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ * Description: print text and return
+ */
+
+int main(void)
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+
+
+	return (0);
+
+}
