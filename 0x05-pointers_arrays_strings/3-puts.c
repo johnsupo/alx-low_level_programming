45a6a9e6232ebf7efdac5484fe06675c6cded8ac
johnsupo <johnsupo@yahoo.com>
2022-09-21 11:33:59 -0700

    3 puts c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - prints string
+ * @str: string
+ */
+void _puts(char *str)
+{
+	int a = 0;
+
+	while (*(str + a) != '\0')
+	{
+		_putchar(*(str + a));
+		a++;
+	}
+	_putchar('\n');
+}
