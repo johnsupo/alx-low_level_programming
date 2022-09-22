f73e3e441669db56a4a3b31305877b6a80ebf3ce
johnsupo <johnsupo@yahoo.com>
2022-09-22 11:34:07 -0700

    main h

---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+04 more functions nested loops readme
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,135 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+/**
+ * _putchar - writes the character c to std
+ * @c: parameter to compare
+ * Return: 1 or 0
+ */
+int _putchar(char c);
+/**
+ * print_alphabet - function to print alphabet
+ */
+void print_alphabet(void);
+/**
+ * print_alphabet_x10 - prints alphabet 10x
+ */
+void print_alphabet_x10(void);
+/**
+ * _islower - prints if c is lowercase
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _islower(int c);
+/**
+ * _isalpha - prints if character is alphabet
+ * @c: character to use
+ * Return: 1 or 0
+ */
+int _isalpha(int c);
+/**
+ * print_sign - prints a sign based on n
+ * @n: integer to input
+ * Return: 1 or 0 or -1
+ */
+int print_sign(int n);
+/**
+ * _abs - gives absolute value
+ * @int: integer
+ * Return: integer or absolute value of int
+ */
+int _abs(int);
+/**
+ * print_last_digit - prints last digit
+ *
+ * @int: integer to input
+ * Return: laste digit of integer
+ */
+int print_last_digit(int);
+/**
+ * jack_bauer - jack bauer
+ */
+void jack_bauer(void);
+/**
+ * times_table - times this up
+ *
+ */
+void times_table(void);
+/**
+ * add - adds stuff up
+ * @int: integers to add
+ * Returns: returns a sum
+ */
+int add(int, int);
+/**
+ * print_to_98 - prints to 98
+ * @n: integer to start at
+ *
+ */
+void print_to_98(int n);
+/**
+ * print_times_table - prints times table
+ * @n: integers to put in this table
+ *
+ */
+void print_times_table(int n);
+/**
+ * _isupper - determines uppercase or not
+ * @c: character to determine
+ * Return: true or false
+ */
+int _isupper(int c);
+/**
+ * _isdigit - checks if it is a digit
+ * @c: character to check
+ * Return: returns boolean
+ */
+int _isdigit(int c);
+/**
+ * mul - multiply numbers
+ * @a: integer to multiply
+ * @b: intiger to multiply
+ * Return: gives result
+ */
+int mul(int a, int b);
+/**
+ * print_numbers - prints the numbers
+ */
+void print_number(void);
+/**
+ * print_most_numbers - prints the numbers
+ */
+void print_most_numbers(void);
+/**
+ * more_numbers - prints more numbers
+ */
+void more_numbers(void);
+/**
+ * print_line - prints a line
+ * @n: integer to print stuff
+ *
+ */
+void print_line(int n);
+/**
+ * print_diagonal - prints diagonal
+ * @n: integer to see how many lines
+ */
+void print_diagonal(int n);
+/**
+ * print_square - prints a square
+ *
+ * @size: prints the size of anything
+ */
+void print_square(int size);
+/**
+ * print_triangle - prints a triangle
+ * @size: size of a triangle
+ */
+void print_triangle(int size);
+/**
+ * print_number - prints number
+ *
+ * @n: integer to print to char
+ */
+void print_number(int n);
+
+#endif
