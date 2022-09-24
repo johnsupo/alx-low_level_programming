60106a3b6bd8e7c251361cdc57d9131092653452
johnsupo <johnsupo@yahoo.com>
2022-09-24 15:53:46 -0700

    101 quote c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: Always 1 (success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(2,
-"and that piece of art is useful\" - Dora Korper, 2015-10-19\n", 59);
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
