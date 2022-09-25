aabb551b29aa15c3d3e12aa8960d9d5d55b3a270
johnsupo <johnsupo@yahoo.com>
2022-09-25 08:28:34 -0700

    8 print base 16 c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,7 +2,7 @@
 /**
  * main - entry point
  *
- * Return: always returns 0
+ * Return: alway returns 0
  *
  */
 int main(void)
@@ -16,7 +16,7 @@ int main(void)
 		a++;
 	}
 	a = '1';
- 	for (i = 0; i < 6; i++)
+	for (i = 0; i < 6; i++)
 	{
 		putchar('0' + a);
 		a++;
