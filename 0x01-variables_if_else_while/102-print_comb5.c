fe6b3f6334ab6b55cc85b93aa23b112fac9a4d38
johnsupo <johnsupo@yahoo.com>
2022-09-25 08:48:20 -0700

    102 print comb 5 c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,14 +2,31 @@
 /**
  * main - entry point
  *
- * Return: always returns 0
+ * Return: alway returns 0
+ *
  */
 int main(void)
 {
-	int a = 0;
+	int i;
+	int j;
 
-	for (; a < 10; a++)
-		putchar('0' + a);
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
 	putchar('\n');
 	return (0);
 }
